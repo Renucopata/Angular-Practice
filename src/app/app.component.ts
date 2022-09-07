9dc48b411157b3c45586609f4b2bbe66913e1cc3
Renucopata <49027215+Renucopata@users.noreply.github.com>
2022-09-07 15:48:41 -0400

    De check practice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,9 @@ import { Component, VERSION } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
+
+
 export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+  public nameAppComponent="Camila";
+  public lastName="Perez";
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { Test1Component } from './test1/test1.component';
+
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent , Test1Component],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
