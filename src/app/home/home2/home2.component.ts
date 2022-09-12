3d057824e00a534f7f5e887203a17000d31b818a
Renucopata <49027215+Renucopata@users.noreply.github.com>
2022-09-12 18:53:01 -0400

    styles

--- src/app/home/home2/home2.component.ts
+++ src/app/home/home2/home2.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home2',
-  templateUrl: './home2.component.html',
-  styleUrls: ['./home2.component.css']
+  template: `
+  <div>
+  <p> Home 2 html in ts </p>
+
+  <button (click)="testButton(null)">Test</button>
+
+  <p> {{arg1 ?? arg2}} </p>
+  <p> {{arg1 || arg2}} </p>
+  </div>
+  `,
+
 })
 export class Home2Component implements OnInit {
+
+  arg1 = 0;
+  arg2 = "El ex de mi novia";
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public testButton(name: string) {
+    const ifNotNull = 'Camila';
+    console.log(name?? ifNotNull); //nullish colaising
+    return name + "Engañado";
   }
 
 }
