71f4bf903149c1f3a2c1b8cfc6a6a0eb88680137
Renucopata <49027215+Renucopata@users.noreply.github.com>
2022-09-13 02:52:35 -0400

    pure and impures pipes anf funcs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,37 @@ import { Component, VERSION } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+
+  numbers: any = [1, 2, 3, 4, 5, 6, 7, 8]
+
+
+constructor() {
+  console.log("Pure Sums")
+  console.log(this.pureSum(2, 3))
+  console.log(this.pureSum(2, 3))
+  console.log(this.pureSum(2, 3))
+  console.log("Impure Sums")
+  console.log(this.impureSum(2))
+  console.log(this.impureSum(2))
+  console.log(this.impureSum(3))
+  console.log(this.impureSum2(2, 3))
+  console.log(this.impureSum2(2, 3))
+  console.log(this.impureSum2(2, 3))
+
+
 }
+
+pureSum = (a, b) => {return a + b}
+impureSum2(a:number, b:number){
+  return (a + b + Math.random()).toFixed()
+}
+
+impureSum = (() => {
+  let state = 0;
+  return (v) => {
+    return state += v;
+  }
+})();
+}
+
+
