d7d577322fe4ce86c19758a9f3a547ef6c4b41de
Renucopata <49027215+Renucopata@users.noreply.github.com>
2022-09-13 10:09:39 -0400

    childrens

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from '../home/home.component';
+import { Home1Component } from '../home/home1/home1.component';
+import { Home2Component } from '../home/home2/home2.component';
 import { UserComponent } from '../user/user.component';
 
 const routes: Routes = [ 
-  {path: 'home', component: HomeComponent},
+  {path: 'home', component: HomeComponent, 
+children: [
+  {path: 'home1', component: Home1Component},
+  {path: 'home2', component: Home2Component}
+]},
   {path: 'user', component: UserComponent}
 ]
 
