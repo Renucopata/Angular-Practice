593d0615df42695772601f2d9c835743e8dc74f7
Renucopata <49027215+Renucopata@users.noreply.github.com>
2022-09-13 14:07:45 -0400

    1 file modified

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -3,15 +3,11 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './admin/admin.component';
 
-const routes: Routes = [
-  {
-    path: '', component: AdminComponent
-  },
-];
+
 
 @NgModule({
   imports: [
-    CommonModule, RouterModule.forChild(routes)
+    CommonModule
   ],
   declarations: []
 })
