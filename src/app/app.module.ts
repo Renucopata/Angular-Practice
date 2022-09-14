d44f35810bcdbac85e3fd9db7cd830aec67a44f4
Renucopata <49027215+Renucopata@users.noreply.github.com>
2022-09-14 01:34:22 -0400

    [29-08-2022 task 3] tailwind

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
+import { MatProgressSpinner } from '@angular/material/progress-spinner';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, BrowserAnimationsModule, MatButtonModule ],
+  imports:      [ BrowserModule, FormsModule, BrowserAnimationsModule, MatButtonModule],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ]
 })
