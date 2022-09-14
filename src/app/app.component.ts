1bb487d2f92c56b8d5e4fd22fea1fd7d91a2825f
Renucopata <49027215+Renucopata@users.noreply.github.com>
2022-09-14 03:47:19 -0400

    [02-09-2022 task 2] POST and iterator patern

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, OnInit, VERSION } from '@angular/core';
+import { ProductService } from './product.service';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
+export class AppComponent implements OnInit {
+
+  constructor(private productService: ProductService) {
+
+  }
+
+
+ngOnInit(): void {
+  this.productService.getAllProducts()
+      .subscribe(res => {
+        console.log('RESPONSE GET: ', res)
+      })
+
+    this.productService.createProduct({
+      description: 'soy la descripcion',
+      imageUrl: 'soy la imagen',
+      ownerId: 1,
+      price: 20,
+      title: 'Polera',
+    }).subscribe(res => {
+      console.log('RESPONSE POST: ', res)
+    })
+
+    this.productService.deleteProduct('p2').subscribe(res => {
+      console.log('RESPONSE DELETE: ', res)
+    })
+
+}
+
+
 
 }
