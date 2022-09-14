c9422020927bf88cb552136fd00c2d41f0f3d820
Renucopata <49027215+Renucopata@users.noreply.github.com>
2022-09-14 04:15:50 -0400

    [05-09-2022 task 1] template driven forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component, VERSION } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormControl, FormGroup} from "@angular/forms";
 
 @Component({
-  selector: 'my-app',
+  selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent implements OnInit {
+  title = 'test';
+  public colorControl = new FormControl('');
+  public personForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+
+    this.personForm = this.formBuilder.group({
+      name: '',
+      lastName: '',
+      age: 0,
+      country: ''
+    });
+
+  }
+  ngOnInit() {
+   
+  }
+  onSubmit(form:any){console.log('FORM: ', form)}
+
 }
